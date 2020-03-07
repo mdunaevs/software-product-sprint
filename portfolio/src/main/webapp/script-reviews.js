bc0aa7fdf52be029c9b22c7d8af347be3f9efefc
Max Dunaevschi <mdunaevs@andrew.cmu.edu>
2020-03-07 18:33:50 -0500

    Condition the cards based on sentiment scores. Only positive.

--- portfolio/src/main/webapp/script-reviews.js
+++ portfolio/src/main/webapp/script-reviews.js
@@ -2,10 +2,12 @@ function printableMessage(comments){
     console.log(comments);
     var s = "";
     for(var i = 0; i < comments.length; i++){
-        s += ("<div class='card grid-item i" + (i + 1) + "'> <img src='images/img_avatar_m.png' alt='Avatar' style='width:100%'> <div class='container'> <h4><b>" 
-        + comments[i]["name"] + "</b></h4> <p>" 
-        + comments[i]["message"] 
-        + "</p> </div> </div>");
+        if(comments[i]["score"] >= 0.0){
+            s += ("<div class='card grid-item i" + (i + 1) + "'> <img src='images/img_avatar_m.png' alt='Avatar' style='width:100%'> <div class='container'> <h4><b>" 
+            + comments[i]["name"] + "</b></h4> <p>" 
+            + comments[i]["message"] 
+            + "</p> </div> </div>");
+        }
         //s += comments[i]["name"] + " says " + comments[i]["message"] + "(" + comments[i]["score"] + ")\n";
     } 
     
@@ -16,6 +18,6 @@ function getRandomQuoteUsingArrowFunctions() {
   fetch('/data')
   .then(response => response.json())
   .then((msg) => {
-    document.getElementById('hello').innerHTML = printableMessage(msg);
+    document.getElementById('cards').innerHTML = printableMessage(msg);
   });
 }
