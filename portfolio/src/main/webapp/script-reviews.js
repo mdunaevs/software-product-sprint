9c555932dde8d1081490afec9df7856e5420639b
Max Dunaevschi <mdunaevs@andrew.cmu.edu>
2020-03-07 18:22:42 -0500

    Dynamic card insertion based on comments

--- portfolio/src/main/webapp/script-reviews.js
+++ portfolio/src/main/webapp/script-reviews.js
@@ -2,16 +2,20 @@ function printableMessage(comments){
     console.log(comments);
     var s = "";
     for(var i = 0; i < comments.length; i++){
-        s += comments[i]["name"] + " says " + comments[i]["message"] + "(" + comments[i]["score"] + ")\n";
+        s += ("<div class='card grid-item i" + (i + 1) + "'> <img src='images/img_avatar_m.png' alt='Avatar' style='width:100%'> <div class='container'> <h4><b>" 
+        + comments[i]["name"] + "</b></h4> <p>" 
+        + comments[i]["message"] 
+        + "</p> </div> </div>");
+        //s += comments[i]["name"] + " says " + comments[i]["message"] + "(" + comments[i]["score"] + ")\n";
     } 
     
-    return s;
+    return s;    
 }
 
 function getRandomQuoteUsingArrowFunctions() {
   fetch('/data')
   .then(response => response.json())
   .then((msg) => {
-    document.getElementById('hello').innerText = printableMessage(msg);
+    document.getElementById('hello').innerHTML = printableMessage(msg);
   });
 }
